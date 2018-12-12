a8df989600bb3b55294e68447bc39c98da015a83
Zoe Ames <zoeames@gmail.com>
2018-12-11 19:36:24 -0600

    added firebase connection

--- src/App/App.js
+++ src/App/App.js
@@ -1,35 +1,20 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
-import logo from './logo.svg';
+
+import connection from '../helpers/data/connection';
+
+import Auth from '../components/Auth/Auth';
+
 import './App.scss';
 
 class App extends Component {
+  componentDidMount() {
+    connection();
+  }
+
   render() {
-    console.log(this);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button className="btn btn-danger">HELP</button>
-          <Button
-            tag="a"
-            color="info"
-            size="large"
-            href="http://google.com"
-            target="_blank"
-          >Reactstrap</Button>
-        </header>
+        <Auth />
       </div>
     );
   }
