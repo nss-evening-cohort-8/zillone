bb21705e723fedf7a3106945651313baf0fe51c0
Zoe Ames <zoeames@gmail.com>
2018-12-15 12:04:04 -0600

    added delete button and fontawesome

--- src/components/ListingItem/ListingItem.js
+++ src/components/ListingItem/ListingItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import listingShape from '../../helpers/propz/listingShape';
 import formatPrice from '../../helpers/formatPrice';
+import authRequests from '../../helpers/data/authRequests';
 
 import './ListingItem.scss';
 
@@ -12,10 +13,27 @@ class ListingItem extends React.Component {
 
   render() {
     const { listing } = this.props;
+    const uid = authRequests.getCurrentUid();
+
+    const makeButtons = () => {
+      if (listing.uid === uid) {
+        return (
+          <div>
+            <span className="col">
+              <button className="btn btn-default">
+                <i className="fas fa-trash-alt"></i>
+              </button>
+            </span>
+          </div>
+        );
+      }
+      return <span className="col-2"></span>;
+    };
     return (
       <li className="listing-item text-center">
         <span className="col-7">{listing.address}</span>
         <span className="col-3">{formatPrice(listing.price)}</span>
+        {makeButtons()}
       </li>
     );
   }
--- src/helpers/data/authRequests.js
+++ src/helpers/data/authRequests.js
@@ -8,7 +8,10 @@ const authenticate = () => {
 
 const logoutUser = () => firebase.auth().signOut();
 
+const getCurrentUid = () => firebase.auth().currentUser.uid;
+
 export default {
   authenticate,
   logoutUser,
+  getCurrentUid,
 };
